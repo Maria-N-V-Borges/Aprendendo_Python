b49c95ff5738bcc03750be3dba721a35de5b05fa
Maria N. V. Borges <teca3vieira@gmail.com>
2025-09-13 14:20:54 -0300

    Create metros.py

---
+++ metros.py
@@ -0,0 +1,8 @@
+#Desafio: Programa para converter metros em centímetros e milímetros 
+
+m = float(input('Digite o valor em metros: '))
+
+c = m * 100
+mi = m * 1000
+
+print(f'{m} metros corresponde a {c} centímetros e {mi} milímetros')
