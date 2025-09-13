8e26a2c48699198e226c83e55ab91843138d452f
Maria N. V. Borges <teca3vieira@gmail.com>
2025-09-13 14:09:27 -0300

    Create media.py

---
+++ media.py
@@ -0,0 +1,9 @@
+#Desafio: Média de duas notas de um aluno
+
+nota1 = float(input('Digite a primera nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+
+media = (nota1 + nota2)/ 2
+
+print(f'A primeira nota foi {nota1}, a segunda nota foi {nota2}.')
+print(f'A média do aluno é {media:.2f}')
