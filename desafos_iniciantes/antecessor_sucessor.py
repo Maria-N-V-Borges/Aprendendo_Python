4d37cd828a748ae350ad1b948beebeb57c935723
Maria N. V. Borges <teca3vieira@gmail.com>
2025-09-13 14:41:33 -0300

    Delete desafos_iniciantes directory

--- desafos_iniciantes/antecessor_sucessor.py
+++
@@ -1,7 +0,0 @@
-#Desafio: Mostrar o sucessor e o antecessor de um número
-n = int(input('Digite um número inteiro: '))
-
-antecessor = n - 1
-sucessor = n + 1
-
-print(f'O número digitado foi {n}. O antecessor é {antecessor} e o sucessor é {sucessor}')
