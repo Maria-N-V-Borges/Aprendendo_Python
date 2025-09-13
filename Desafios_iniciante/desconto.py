a43aafadb38ec51ffabe4311e911e50880a839c5
Maria N. V. Borges <teca3vieira@gmail.com>
2025-09-13 15:37:13 -0300

    Create desconto.py

---
+++ Desafios_iniciante/desconto.py
@@ -0,0 +1,9 @@
+#Desafio: Clacular o preço com desconto
+
+p = float(input('Digite o preço do produto: R$ '))
+
+d = p * 0.05
+novo_p = p - d
+
+print(f'O preço original era R${p:.2f}')
+print(f'Com 5% de desconto, o novo preço é R${novo_p:.2f}')
