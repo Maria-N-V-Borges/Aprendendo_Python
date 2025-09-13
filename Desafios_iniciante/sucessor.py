d71be9935c93c5ec4312494011e5b9ad8dcecb3d
Maria N. V. Borges <teca3vieira@gmail.com>
2025-09-13 14:44:07 -0300

    Update sucessor.py

--- Desafios_iniciante/sucessor.py
+++ Desafios_iniciante/sucessor.py
@@ -1,1 +1,7 @@
+#Desafio: Mostrar o sucessor e o antecessor de um número
+n = int(input('Digite um número inteiro: '))
 
+antecessor = n - 1
+sucessor = n + 1
+
+print(f'O número digitado foi {n}. O antecessor é {antecessor} e o sucessor é {sucessor}')
