b20ed0a90a91ff2ab1ddd4bd41ffe72387010209
Maria N. V. Borges <teca3vieira@gmail.com>
2025-09-15 14:56:39 -0300

    Update and rename salario.py to desafio013_salario.py

--- Desafios_iniciante/desafio013_salario.py
+++ Desafios_iniciante/desafio013_salario.py
@@ -5,5 +5,5 @@ s = float(input('Digite o salário do funcionário: R$ '))
 a = s * 0.15
 novo_s = s + a
 
-print(f'Salário original: R${s:.3f}')
-print(f'Com 15% de aumento, o novo salário é R${novo_s:.3f}')
+print(f'Salário original: R${s:.2f}')
+print(f'Com 15% de aumento, o novo salário é R${novo_s:.2f}')
