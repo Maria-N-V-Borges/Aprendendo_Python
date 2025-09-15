264e2bfdc47a17a685e31adaafa4f3ac9e8556da
Maria N. V. Borges <teca3vieira@gmail.com>
2025-09-15 15:53:05 -0300

    Update desafio026_A.py

--- Desafios_iniciante/desafio026_A.py
+++ Desafios_iniciante/desafio026_A.py
@@ -11,3 +11,9 @@ u = f.rfind('A') + 1
 print(f'A letra A aparece {q} vezes na frase.')
 print(f'A primeira letra A aparece a posição {p}.')
 print(f'A última letra A aparece na posição {u}.')
+
+#.upper() → deixa tudo maiúsculo, pra não dar problema se a pessoa digitar a minúsculo.
+#.count("A") → conta quantos “A” tem.
+#.find("A") → pega a posição do primeiro “A”.
+#.rfind("A") → pega a posição do último “A”.
+#O +1 é só pra mostrar a posição de forma humana (começando em 1 e não em 0).
