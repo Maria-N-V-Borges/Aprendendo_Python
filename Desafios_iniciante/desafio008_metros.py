6c229167534d442db41cf206c2d407c8993c5bf6
Maria N. V. Borges <teca3vieira@gmail.com>
2025-09-15 14:41:51 -0300

    Update and rename metros.py to desafio008_metros.py

--- Desafios_iniciante/desafio008_metros.py
+++ Desafios_iniciante/desafio008_metros.py
@@ -6,3 +6,6 @@ c = m * 100
 mi = m * 1000
 
 print(f'{m} metros corresponde a {c} centímetros e {mi} milímetros')
+
+#Multiplicamos por 100 para obter centímetros, porque 1 metro = 100 cm
+#Miltiplicamos por 1000 para obter milímetros, porque 1 metro = 1000 cm
