d9ce7302c648b8ba9eb70057a9ef7e5b4f845513
Maria N. V. Borges <teca3vieira@gmail.com>
2025-09-15 14:30:54 -0300

    Update and rename dobro_triplo_raiz.py to desafio006_dobro_triplo_raiz.py

--- Desafios_iniciante/desafio006_dobro_triplo_raiz.py
+++ Desafios_iniciante/desafio006_dobro_triplo_raiz.py
@@ -10,3 +10,7 @@ print(f'O número digitado foi {n}')
 print(f'O dobro é {dobro}')
 print(f'O triplo é {triplo}')
 print(f'A raiz quadrada é {raiz:.2f}')
+
+#n ** 0.5 é a forma rápida de calcular a raiz quadrada
+#pow(n, 0.5) também funciona
+#:.2f no final da f-string serve para mostrar a raiz com 2 casas decimais (ex: 4.00)
