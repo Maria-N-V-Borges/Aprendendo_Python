5add87f80641815d5545ac72512e412912bd3a20
Maria N. V. Borges <teca3vieira@gmail.com>
2025-09-15 16:02:12 -0300

    Update desafio027_primeiro_ultimo_nome.py

--- Desafios_iniciante/desafio027_primeiro_ultimo_nome.py
+++ Desafios_iniciante/desafio027_primeiro_ultimo_nome.py
@@ -9,3 +9,8 @@ u = partes[-1]
 
 print(f'Primeiro nome: {primeiro}')
 print(f'Último nome: {u}')
+
+#.strip() → tira espaços extras no início e fim.
+#.split() → divide o nome em uma lista (cada palavra vira um item).
+#partes[0] → pega o primeiro item da lista (primeiro nome).
+#partes[-1] → pega o último item da lista (último nome).
