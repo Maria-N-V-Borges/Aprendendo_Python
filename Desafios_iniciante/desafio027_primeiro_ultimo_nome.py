ecd387871ce53b2ba8a844c296b167711fc0c747
Maria N. V. Borges <teca3vieira@gmail.com>
2025-09-15 16:00:59 -0300

    Create desafio027_primeiro_ultimo_nome.py

---
+++ Desafios_iniciante/desafio027_primeiro_ultimo_nome.py
@@ -0,0 +1,11 @@
+#Desafio: Programa para mostrar primeiro e último nome
+
+n = input('Digite seu nome completo: ').strip()
+
+partes = n.split()
+
+primeiro = partes[0]
+u = partes[-1]
+
+print(f'Primeiro nome: {primeiro}')
+print(f'Último nome: {u}')
