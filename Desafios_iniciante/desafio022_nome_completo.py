f452636272ce4e2b4f7a547532790da5a9240db5
Maria N. V. Borges <teca3vieira@gmail.com>
2025-09-15 13:44:14 -0300

    Update desafio022_nome_completo.py

--- Desafios_iniciante/desafio022_nome_completo.py
+++ Desafios_iniciante/desafio022_nome_completo.py
@@ -14,3 +14,10 @@ print(f'Nome em maiúscula: {ma}')
 print(f'Nome em minúsculas: {mi}')
 print(f'Total de letras (sem espaços): {total_letras}')
 print(f'O primeiro nome é {primeiro_nome} e ele tem {letras_primeiro} letras')
+
+
+#.strip() remove espaços extras no inicio e no fim
+#.upper() deixa tudo MAIÚSCULO
+#.lower() deixa tudo minúsculo
+#.replace(' ','') remove os espaços pra conter só as letras
+#.split()[0] pega a primera palavra do nome, ou seja, o primeiro nome.
