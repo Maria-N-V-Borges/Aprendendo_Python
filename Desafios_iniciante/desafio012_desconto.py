197d7efff938a04cfffefb923ea4dce506398bf3
Maria N. V. Borges <teca3vieira@gmail.com>
2025-09-15 14:52:58 -0300

    Update and rename desconto.py to desafio012_desconto.py

--- Desafios_iniciante/desafio012_desconto.py
+++ Desafios_iniciante/desafio012_desconto.py
@@ -7,3 +7,6 @@ novo_p = p - d
 
 print(f'O preço original era R${p:.2f}')
 print(f'Com 5% de desconto, o novo preço é R${novo_p:.2f}')
+
+#Multiplicamos por 0.05 para calcular 5% do preço
+#Subtraimos do valor original para obter o novo preço.
