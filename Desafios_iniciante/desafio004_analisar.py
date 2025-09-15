a126ee9686fae3f659a279eae23dfcd72bc94a1f
Maria N. V. Borges <teca3vieira@gmail.com>
2025-09-15 14:27:49 -0300

    Update desafio004_analisar.py

--- Desafios_iniciante/desafio004_analisar.py
+++ Desafios_iniciante/desafio004_analisar.py
@@ -10,3 +10,11 @@ print(f'É alfanumérico? {algo.isalnum()}')
 print(f'Está em maiúscula? {algo.isupper()}')
 print(f'Está em minúscula? {algo.islower()}')
 print(f'Está capitalizado (primeira letra maiúscula)? {algo.istitle()}')
+
+#type(algo) mostra o tipo do dado (mas cuidado: input() sempre lê como string).
+#.isspace() → verifica se tem só espaços.
+#.isnumeric() → diz se é só número.
+#.isalpha() → diz se é só letras.
+#.isalnum() → letras e números misturados.
+#.isupper() / .islower() → maiúsculo ou minúsculo.
+#.istitle() → se está como “Título” (primeira letra maiúscula).
