36ff4098ba45ef31bad79d5ca152235eae90e800
Maria N. V. Borges <teca3vieira@gmail.com>
2025-09-15 13:58:44 -0300

    Create desafio002_boasvindas.py

---
+++ Desafios_iniciante/desafio002_boasvindas.py
@@ -0,0 +1,8 @@
+#Desafio: Programa de boas-vindas
+
+n = input('Digite seu nome: ')
+
+print(f'Seja bem-vindo(a), {n}! 💖✨ É um prazer ter você aqui!')
+
+#input() pega o nome que a pessoa digita
+# O f-string (f'...') deixa a mensagem personalizada com o nome da pessoa
