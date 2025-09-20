87b44856c79f38da42b952630d8736e005603cc7
Maria N. V. Borges <teca3vieira@gmail.com>
2025-09-20 13:52:45 -0300

    Create desafio047_par_1_e_50.py

---
+++ mundo2/desafio047_par_1_e_50.py
@@ -0,0 +1,8 @@
+#Desafio: Programa para mostrar os números pares entre 1 e 50
+
+print('Número pares de 1 até 50: ')
+
+for num in range(2, 51, 2): # começa em 2, vai até 50, pulando de 2 em 2
+    print(num, end=' ') # end=' ' mostra os números lado a lado
+
+print('\n✅ Fim da contagem!')
