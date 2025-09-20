cb6255e6f6090a1dd7a393820f53d1f94c5bcf1d
Maria N. V. Borges <teca3vieira@gmail.com>
2025-09-20 13:37:56 -0300

    Create desafio046_fogos.py

---
+++ mundo2/desafio046_fogos.py
@@ -0,0 +1,11 @@
+#Desafio: contagem regressiva Ano Novo
+
+import time
+
+print('🥳 Contagem Regressiva para os Fogos 🎆')
+
+for i in range(10, -1, -1): # começa em 10, termina em 0, de -1 em -1
+    print(i)
+    time.sleep(1)
+
+print('🥳🥳🥳 BOOOOOM! Feliz Ano Novo 🎆🎆🎆')
