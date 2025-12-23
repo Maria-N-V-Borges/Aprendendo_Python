343b08c32e840af20224b1e96586e64d431d0ae0
Maria N. V. Borges <m.nazare.borges.cc@gmail.com>
2025-12-23 13:01:03 -0300

    Create desafio074_cincoNumeros.py

---
+++ mundo3/desafio074_cincoNumeros.py
@@ -0,0 +1,13 @@
+from random import randint
+
+numeros = (
+    randint(1, 10),
+    randint(1, 10), 
+    randint(1, 10), 
+    randint(1, 10), 
+    randint(1, 10)
+    )
+
+print('Os números sorteados foram:', numeros)
+print(f'O maior valor sorteado foi: {max(numeros)}')
+print(f'O menor valor sorteado foi: {min(numeros)}')
