6b5e2a934e9bbb23d90bab6603c9f524b92f6ec5
Maria N. V. Borges <m.nazare.borges.cc@gmail.com>
2026-02-10 16:10:02 -0300

    Create main.py

---
+++ mundo3/desafio113/main.py
@@ -0,0 +1,7 @@
+from moeda import leiaInt, leiaFloat
+
+inteiro = leiaInt("Digite um número inteiro: ")
+real = leiaFloat("Digite um número real: ")
+
+print(f"O valor inteiro digitado foi {inteiro}")
+print(f"O valor real digitado foi {real}")
